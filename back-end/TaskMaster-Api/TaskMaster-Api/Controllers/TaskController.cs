5e1901e572b7ee837fc42568326d7acc57020d90
zuoyuan <zuoyuan-zhao@outlook.com>
2023-04-22 14:42:01 -0500

    controller added

--- back-end/TaskMaster-Api/TaskMaster-Api/Controllers/TaskController.cs
+++ back-end/TaskMaster-Api/TaskMaster-Api/Controllers/TaskController.cs
@@ -1,31 +1,100 @@
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.Runtime;
 using Microsoft.AspNetCore.Mvc;
+using TaskMaster_Api.DaoLayer;
 using TaskMaster_Api.Models;
 
 namespace TaskMaster_Api.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class TaskController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        private readonly TaskDao _taskDao;
 
         private readonly ILogger<TaskController> _logger;
 
+        private readonly AmazonDynamoDBClient client;
+
         public TaskController(ILogger<TaskController> logger)
         {
+            var credentials = new BasicAWSCredentials(accessKey, secretKey);
+            var region = RegionEndpoint.USEast2; 
+            client = new AmazonDynamoDBClient(credentials, region);
+            _taskDao = new TaskDao(client);
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetTasks")]
-        public IEnumerable<TaskEntry> Get()
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskEntry>>> GetTasks()
         {
-            return Enumerable.Range(1, 5).Select(index => new TaskEntry
+            var tasks = await _taskDao.GetTasksAsync();
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskEntry>> GetTask(string id)
+        {
+            var task = await _taskDao.GetTaskAsync(id);
+
+            if (task == null)
             {
-            })
-            .ToArray();
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TaskEntry>> CreateTask(TaskEntry task)
+        {
+            task.Id = Guid.NewGuid().ToString();
+            task.CreatedDate = DateTime.UtcNow;
+            task.ModifiedDate = DateTime.UtcNow;
+            task.VersionNumber = 1;
+
+            await _taskDao.AddTaskAsync(task);
+            return Ok(task);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateTask(string id, TaskEntry task)
+        {
+            var existingTask = await _taskDao.GetTaskAsync(id);
+
+            if (existingTask == null)
+            {
+                return NotFound();
+            }
+
+            existingTask.Title = task.Title;
+            existingTask.Description = task.Description;
+            existingTask.DueDate = task.DueDate;
+            existingTask.Status = task.Status;
+            existingTask.Priority = task.Priority;
+            existingTask.ModifiedDate = DateTime.UtcNow;
+            existingTask.AssignedTo = task.AssignedTo;
+            existingTask.Notes = task.Notes;
+            existingTask.Tags = task.Tags;
+            existingTask.VersionNumber++;
+
+            await _taskDao.UpdateTaskAsync(existingTask);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteTask(string id)
+        {
+            var existingTask = await _taskDao.GetTaskAsync(id);
+
+            if (existingTask == null)
+            {
+                return NotFound();
+            }
+
+            await _taskDao.DeleteTaskAsync(id);
+            return NoContent();
         }
     }
 }
